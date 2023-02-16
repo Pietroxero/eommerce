f4492f8af75e0579404cd4f04b4d918e0c79db97
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-02-16 11:08:29 -0800

    added in try and catch errs within product routes js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,9 +22,20 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk (req.params.id, {include: [{ model: Category }, { model: Tag}],
+    });
+    if (!productData) {
+      res.status (404).json ({ message: 'no matches found for ID'});
+    }
+    res.status (200).json (productData)
+  }
+  catch (err) {
+    res.status (500).json (err)
+  }
 });
 
 // create new product
@@ -101,8 +112,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy ({
+where: {
+  id: req.params.id,
+},
+    })
+    if (!deleteProduct) {
+      res.status (400).json ({ message: 'No match found' })
+      return
+    }
+    res.status (200).json (deleteProduct)
+}
+catch (err) {
+  res.status (400).json (err)
+}
 });
 
 module.exports = router;
